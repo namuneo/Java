0b9ac6715f61b0188c0d3d004428c3a5041f242e
namuneo <rlatjdwns8934@gmail.com>
2023-12-18 06:16:57 +0900

    반복문 for & while

---
+++ java-start/src/loop/ex/ForEx2.java
@@ -0,0 +1,10 @@
+package loop.ex;
+
+public class ForEx2 {
+
+    public static void main(String[] args) {
+        for (int i = 2, count = 1; count <= 10; i += 2, count++) {
+            System.out.println(i);
+        }
+    }
+}
