f456bd11e15c1474303ae910e828563a748abb74
namuneo <rlatjdwns8934@gmail.com>
2023-12-19 07:31:43 +0900

    scanner 공부완료

---
+++ java-start/src/scanner/Scanner1.java
@@ -0,0 +1,14 @@
+package scanner;
+
+import java.util.Scanner;
+
+public class Scanner1 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("문자열을 입력하세요:");
+        String str = scanner.nextLine(); // 입력을 String으로 가져온다.
+        System.out.println("입력한 문자열 : " + str);
+    }
+}
